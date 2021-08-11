198386cf4e7d40190ca369acf2f3bb796bdde5da
Lucas USER Dani <67432077+lucascassimirosena@users.noreply.github.com>
2021-08-11 15:23:30 -0300

    solved the Sum Challenge

--- HackerRank/Sum_and_Difference_of_Two_Numbers/Sum.c
+++ HackerRank/Sum_and_Difference_of_Two_Numbers/Sum.c
@@ -18,13 +18,20 @@ Print the sum and difference of two float variable rounded to one decimal place 
     {
         int a;
         int b;
-        int c;
-        int d;
+        float c;
+        float d;
 
-        scanf("%d %d", &a, &b);
-        c = a + b;
-        d = a - b;
-        printf("%d %d", c, d);
+        scanf("%d %d %f %f", &a, &b, &c, &d);
+        printf("%d %d\n%.1f %.1f", a+b, a-b, c+d, c-d);
 
         return 0;
     }
+
+    //EXPLANATION
+    /* First it was necessary to declare de variables (lines 19-22); then
+    we use scanf() to scan the user's input from standard input. We need to tell the
+    computer what kind of input we're scanning and that's why we need to use %d for int
+    and %f for float. & will pint to the value in the memory adress.
+
+    moving forward on line 25 we print the first 2 values, press entre \n and then print the float 
+    valus rounded to one decimal number, that's why we need to put .1.*/
