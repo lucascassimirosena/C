94b0e707dba6bbe8fcdd696e3ffecbe1539bffe5
Lucas USER Dani <67432077+lucascassimirosena@users.noreply.github.com>
2021-08-16 17:47:45 -0300

    added some notes about the language

--- Studies/Variables_DataTypes/Variables.c
+++ Studies/Variables_DataTypes/Variables.c
@@ -19,13 +19,13 @@ Bits in memory are grouped into sets of digits (byte)
 So... in short, memory consists of a large number of bits that are grouped in 8(bytes) and has an 
 unique address.
 
-Variables are the names you give to computer memory locations which are used to stores values in a 
+Variables are the names you give to computer memory locations which are used to store values in a 
 computer program and as the name suggests, the value it represents is going to change, differntly from 
 Constants that are... you know, constants, they're going to hold the same value forever. 
 
 In C, all the names must begin with a letter or underscore and can be followed by numbers.
 
-Decalring a variable is when you specify the type of the variable followed by the variable name.
+Declaring a variable is when you specify the type of the variable followed by the variable name.
 C requires that all program variables be declared before they're used in a program.*/
 
 #include <stdio.h>
@@ -38,3 +38,5 @@ int main()
     lucas = 28; //Initialized the variable, I assigned the value 28 to the variable lucas.
     return 0;
 }
+
+/* the difference between the types is in the amount of memory they occupy and the range of values tehy can hold.
