fb91cc6558b3875409fe28562330d3bd88d15d67
Lucas USER Dani <67432077+lucascassimirosena@users.noreply.github.com>
2021-09-02 11:41:13 -0300

    I tried to study C, I made some notes but I couldn't proceed

---
+++ Studies/Command_line/Command.c
@@ -0,0 +1,20 @@
+/* Command Line Arguments 
+
+Sometimes when I program is created that recquires the developer to enter a small amount of information
+at the terminal.
+
+This information must consist of a number indicating the triangular number that you want to have 
+calculated or a word that you want to have looked up in a dictionary
+
+Two ways of handling this:
+    Requesting the data from the user
+    Supply the information to the program at the time the program is executed (command-line arguments)
+
+We know that the main() function is a special function in C
+    Entry point of the program
+
+When main() is called by the runtime system, two arguments are actually passed to the function
+    The first argument (argc for the argument count) is an interger value that specifies the number of
+    arguments typed on the command line.
+    The second argument (argv for argument vector) is an array of characters pointers (strings)
+
