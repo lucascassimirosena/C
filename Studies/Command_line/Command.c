fd8accfb325976962762d518ae67ef10fe10cff8
Lucas USER Dani <67432077+lucascassimirosena@users.noreply.github.com>
2021-09-13 08:08:15 -0300

    Learned about command lines

--- Studies/Command_line/Command.c
+++ Studies/Command_line/Command.c
@@ -1,23 +1,37 @@
 /* Command Line Arguments 
 
-Sometimes when I program is created that recquires the developer to enter a small amount of information
+Sometimes when a program is created that recquires the developer to enter a small amount of information
 at the terminal.
 
 This information must consist of a number indicating the triangular number that you want to have 
-calculated or a word that you want to have looked up in a dictionary
+calculate or a word that you want to have looked up in a dictionary
 
 Two ways of handling this:
-    Requesting the data from the user
-    Supply the information to the program at the time the program is executed (command-line arguments)
+    - Requesting the data from the user
+    - Supply the information to the program at the time the program is executed (command-line arguments)
 
 We know that the main() function is a special function in C
-    Entry point of the program
+    - Entry point of the program
 
 When main() is called by the runtime system, two arguments are actually passed to the function
-    The first argument (argc for the argument count) is an interger value that specifies the number of
+    - The first argument (argc for the argument count) is an interger value that specifies the number of
     arguments typed on the command line.
-    The second argument (argv for argument vector) is an array of characters pointers (strings)
+    - The second argument (argv for argument vector) is an array of characters pointers (strings)
 
-
-CONTINUE
+The first entry in this array is a pointer to the name of the program that is executing.
 */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int numberOfArguments = argc;
+    char *argument1 = argv[0];
+    char *argument2 = argv[1];
+
+    printf("Number of Arguments: %d\n", numberOfArguments);
+    printf("Argument 1 is the program name: %s\n", argument1);
+    printf("Argument 2 is the command line argument: %s\n", argument2);
+
+    return 0;
+}
