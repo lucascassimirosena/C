7ba86f1571bdb09ec5e39a6d0f0fb8238c535f1e
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-03-23 18:19:32 -0300

    added some folders to notes

---
+++ Udemy/1/exercicios/47.c
@@ -0,0 +1,15 @@
+//Ex47
+/* Leia um número inteiro de 4 dígitos (de 1000 a 9999) e imprima 1
+digito por linha.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    printf("Digite o numero: ");
+    scanf("%d", &numero);
+
+    printf("%d\n", numero);
+}
