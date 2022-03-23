338597087162dd20fbb0714a734aac26e78f7b22
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-03-23 08:52:11 -0300

    just renamed it

---
+++ Udemy/1/exercicios/35.c
@@ -0,0 +1,14 @@
+//Ex35
+/*
+Sejam a e b os catetos de um triângulo, onde a hipotenusa é
+obrtida pela equação: hipotenusa = raiz de a2 + b2. 
+Faça um programa que receba os valores de a e b e calcule
+o valor da hipotenusa através da equação. Impprima o resultado dessa operação.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    
+}
