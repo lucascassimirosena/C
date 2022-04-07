86aca7cd10b2523233229dfca67d336eaaa5d612
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-07 16:57:31 -0300

    studying..

--- Udemy/1/notas/7.Vetores/pt1.c
+++ Udemy/1/notas/7.Vetores/pt1.c
@@ -8,7 +8,7 @@ int main()
     char nome[50];
     printf("Qual seu nome? ");
     gets(nome);
-    printf("Olá %s", nome);
+    printf("Ola %s", nome);
 
     //vetores e caracteres
     char letras[26];
@@ -25,10 +25,24 @@ int main()
     }
 
     //vetores de inteiros
-    int numeros[10];
-
+    int numeros[5];
+    numeros[0] = 1;
+    numeros[1] = 3;
+    numeros[2] = 5;
+    numeros[3] = 7;
+    numeros[4] = 9;
+  
     //vetores e reais
     float valores[5];
+    for(int i = 0; i < 5; i++)
+    {
+        valores[i] = (float)numeros[i] / (float)2;
+    }
+
+    for(int i = 4; i >= 0; i--)
+    {
+        printf("%.2f\n", valores[i]);
+    }
 
 
     return 0;
