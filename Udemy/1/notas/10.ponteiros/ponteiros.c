cf34290328852c4183fcb2b1de1ef89896e33ba2
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-12 20:35:18 -0300

    added notes

---
+++ Udemy/1/notas/10.ponteiros/ponteiros.c
@@ -0,0 +1,29 @@
+
+
+#include <stdio.h>
+
+void incrementa(int* contador)
+{
+    printf("Antes de incrementar.\n");
+    printf("O contador vale %d\n", valor);
+    
+
+    printf("Depois de incrementar. \n");
+    printf("O contador vale %d", ++valor);
+}
+
+int main()
+{   /* Quando declaramos uma variável, a linguagem C aloca
+    um espaço na memória para colocar este valor */
+    int contador = 10;
+
+    printf("Antes de incrementar. \n");
+    printf("O contador vale %d\n", contador);
+    printf("O endereco de memoria e %d\n", &contador);
+
+    incrementa(contador);
+
+    printf(" Depois de incrementar. \n");
+    printf("O contador vale %d", contador);
+
+}
