4046d6aeae036bca3cd79308a95e5e93545f1485
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-28 20:41:57 -0300

    working on it

--- Udemy/1/exercicios/Section4/12.c
+++ Udemy/1/exercicios/Section4/12.c
@@ -3,3 +3,21 @@
 Ler um número inteiro. Se o número lido for negativo, escreva a mensagem "Número Inválido"
 Se o número for postivo, caluclar o logarítimo deste número.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    if(num < 0)
+    {
+        printf("Numero invalido.");
+    }
+    else
+    {
+
+    }
+}
+// working on it
