b4c1b9970f898e8649ec2aab9b712f568c08a8d2
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-29 14:52:52 -0300

    solved ex16

---
+++ Udemy/1/exercicios/Section4/16.c
@@ -0,0 +1,71 @@
+//Ex16
+/*
+Usando switch, escreva um programa que leia um inteiro entre 1 e 12 e 
+imprima o mês correspondente a este número. Isto é, janeiro se 1,
+fevereiro se 2, e assim por diante.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int mes;
+
+    printf("Digite um numero entre 1 e 12: ");
+    scanf("%d", &mes);
+
+    switch(mes)
+    {
+        case 1:
+        printf("Janeiro.");
+        break;
+
+        case 2:
+        printf("Fevereiro.");
+        break;
+
+        case 3:
+        printf("Marco.");
+        break;
+
+        case 4:
+        printf("Abril.");
+        break;
+
+        case 5:
+        printf("Maio.");
+        break;
+
+        case 6:
+        printf("Junho.");
+        break;
+
+        case 7:
+        printf("Julho.");
+        break;
+
+        case 8:
+        printf("Agosto.");
+        break;
+
+        case 9:
+        printf("Setembro.");
+        break;
+
+        case 10:
+        printf("Outubro.");
+        break;
+
+        case 11:
+        printf("Novembro.");
+        break;
+
+        case 12:
+        printf("Dezembro.");
+        break;
+
+        default:
+        printf("Numero invalido.");
+        break;
+    }
+}
