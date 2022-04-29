2a06d518977d787e461d767ce4979832a0dc46db
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-29 19:44:48 -0300

    solving

---
+++ Udemy/1/exercicios/Section4/19.c
@@ -0,0 +1,20 @@
+//Ex19
+/*
+Faça um programa para verificar se um determinado número inteiro e divisível por 3
+ou 5, mas não simultaneamnete pelos dois.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if(num % 5 == 0)
+    {
+        printf("Divisivel por 5");
+    }
+}
