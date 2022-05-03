856819d77dde51ff49ff7a49b4065694afc7065b
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-03 13:22:07 -0300

    deleted example repository

---
+++ Udemy/1/exercicios/Section10/01.c
@@ -0,0 +1,16 @@
+//Ex01
+/*
+Escreva um programa que declare um inteiro, um real e um char, e ponteiros
+para inteiro, real, e char. Associe as variáveis aos ponteiros (use &). Modifique
+os valores de cada variável usando os ponteiros. Imprima os valores das variáveis
+antes e após a modificação.
+*/
+
+#include<stdio.h>
+
+void ft_ints(int n, float m, char o)
+{
+    int *n;
+    float *m;
+    char *o;
+}
---
+++ Udemy/1/exercicios/Section4/29.c
@@ -0,0 +1,39 @@
+//Ex29
+/*
+Faça uma prova de matemática pra crianças que estão aprendendo a somar
+números inteiros menores do que 100. Escolha números aleatórios entre 1 e 100,
+e mostre na tela a pergunta: qual é a soma de a + b, onde a e b são os números
+aleatórios. Peça a resposta. Faça cinco perguntas ao aluo, e mostre apra ele as pergutnas e 
+as respostas corretas, além de quantas vezes o aluno acertou.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int n1, n2;
+
+    while(i <= 10)
+    {
+        n1 = rand() % 100;
+        n2 = rand() % 100;
+        printf("Qual e a soma de %d + %d?\n", n1, n2);
+        int answer;
+        scanf("%d", &answer);
+
+        int sum;
+        sum = n1 + n2;
+        if(answer == sum)
+        {
+            printf("Correto!");
+        }
+        else
+        {
+            printf("Errado!");
+        }
+        i++;
+    }
+
+}
