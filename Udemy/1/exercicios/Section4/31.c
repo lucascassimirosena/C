6bbe6b224f1f2a9ae91e576109b55a03e655dda6
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-03 22:32:44 -0300

    solving

--- Udemy/1/exercicios/Section4/31.c
+++ Udemy/1/exercicios/Section4/31.c
@@ -1,1 +1,30 @@
-testing
+//Ex31
+/*
+Faça um programa que receba a altura e o peso de uma pessoa. De acordo com a tabela
+a seguir, verifique e mostre qual a classificação dessa pessoa.
+
+Altura                       Peso
+                    Até 60      Entre 60 e 90 (inclusive)   Acima de 90
+Menor que 120        A              D                           G
+De 1.20 a 1.70       B              E                           H
+Maior que 1.70       C              F                           I
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    float weight, height;
+
+    printf("Coloque o peso: ");
+    scanf("%f", &weight);
+    printf("Coloque a altura: ");
+    scanf("%f", &height);
+
+        if()
+
+
+}
+
+
+//Finish it
