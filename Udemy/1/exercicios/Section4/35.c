d9f9b036b6ed57e43a3d221f8314b572ad180365
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-04 14:57:30 -0300

    just added the ex

---
+++ Udemy/1/exercicios/Section4/35.c
@@ -0,0 +1,6 @@
+//Ex35
+/*
+Leia uma data e determine se ela é valida. Ou seja, verifique se o mês está
+entre 1 e 12, e se o dia existe naquele mês. Note que Feveriro tem 29 dias
+em anos bissextos, e 28 em anos não bissextos.
+*/
