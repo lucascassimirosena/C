8a4b2dda87f71cca50a11b947967ea71b27ab858
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-04 13:06:57 -0300

    solved ex31

--- Udemy/1/exercicios/Section4/31.c
+++ Udemy/1/exercicios/Section4/31.c
@@ -16,15 +16,51 @@ int main()
 {
     float weight, height;
 
-    printf("Coloque o peso: ");
-    scanf("%f", &weight);
+
     printf("Coloque a altura: ");
     scanf("%f", &height);
+    printf("Coloque o peso: ");
+    scanf("%f", &weight);
 
-        if()
+        if(height < 1.20 && weight < 60)
+        {
+            printf("A");
+        }
+        else if(height < 1.20 && (weight >= 60 && weight <= 90))
+        {
+            printf("D");
+        }
+        else if(height < 1.20 && weight > 90)
+        {
+            printf("G");
+        }
 
-
+        else if((height >= 1.20 && height <= 1.70) && (weight < 60))
+        {
+            printf("B");
+        }
+        else if((height >= 1.20 && height <= 1.70) && (weight >= 60 && weight <= 90))
+        {
+            printf("E");
+        }
+        else if((height >= 1.20 && height <= 1.70) && weight > 90)
+        {
+            printf("H");
+        }
+        else if(height > 70 && weight < 60)
+        {
+            printf("C");
+        }
+        else if(height > 70 && (weight >= 60 && weight <= 90))
+        {
+            printf("F");
+        }
+        else if(height > 70 && weight > 90)
+        {
+            printf("I");
+        }
+        else
+        {
+            printf("Invalido");
+        }
 }
-
-
-//Finish it
