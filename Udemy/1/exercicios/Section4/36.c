3afefaa54476ce0f96ab20d92fac6a4f3779aa76
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-06 13:05:56 -0300

    started

---
+++ Udemy/1/exercicios/Section4/36.c
@@ -0,0 +1,10 @@
+//Ex36
+/*
+Escreva um programa que, dado o valor da venda, imprima a comissão que deverá
+ser paga ao vendedor. Para calcular a comissão, considere a tabela abaixo:
+
+Venda Mensal:                                   Comissão:
+
+
+
+*/
