03133db0a4d74b2ad9fae081b083ce8d15a979de
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-06 22:29:47 -0300

    started the exercise

---
+++ Udemy/1/exercicios/Section4/37.c
@@ -0,0 +1,29 @@
+//Ex37
+/*
+As tarifas de certo parque de esctacionamento são as seguintes:
+        1ª e 2ª hora - R$1,00 cada
+        3ªa e 4ª hora - R$1,40 cada
+        5ª hora e seguintes - R$2,00 cada
+
+O número de horas a pagar é sempre inteiro e arredondado por excesso. Deste modo,
+quem estacionar durantee 61 minutos pagará por duas horas, que é o mesom que pagaria
+se tivesse permanecido por 120 minutos. Os momentos de chegada ao parque e partida
+deste são apresentados na forma de pares de inteiros,  representando horas e minutos.
+Por exemplo, o par 12 50 respresentará "dez para uma da tarde". Pretende-se criar um
+programa que, lidos pelo teclado os momentos de chegada e de partida, escreva na tela
+o preço cobrado pelo estacionamento. Admite-se que a chegada e a partida se dão com
+intervalo não superior a 24 horas. Portanto, se uma dada hora de chegada for superior à
+da partida, isso não é uma situação de erro, antes significará que a partida ocorreu no
+dia seguinte ao da chegada.
+
+Leia uma data de nascimento de uma pessoa fornecida através de três números inteiros:
+Dia, Mês e Ano. Teste a validade desta data para saber se esta é uma data válida. Teste 
+se o dia fornecido é um dia válido: dia > 0, dia <= 28 para o mês de fevereiro (29 se o
+ano for bissexto), dia <= 30 em abril, junho, setembro e novembro, dia <= 31 nos outros 
+meses. Teste a validade do mês: mês > 0 e mês < 13. Teste a validade do ano: ano <= ano
+atual (use uma constante definida com o valor igual a 2008). Imprimir: "data válida" ou
+"data inválida" no final da execução do programa.
+*/
+
+
+
