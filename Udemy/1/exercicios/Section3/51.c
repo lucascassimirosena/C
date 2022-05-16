4310b98ab458e4f5bd7d91fca5dc4d1fc92904a6
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-16 18:36:19 -0300

    solving

---
+++ Udemy/1/exercicios/Section3/51.c
@@ -0,0 +1,5 @@
+//Ex51
+/*
+Escreva um programa que leia as coodrdenadas x e y de pontos no R² e calcule
+sua distância da origem (0,0).
+*/
---
+++ Udemy/1/exercicios/Section3/53.c
@@ -0,0 +1,6 @@
+//Ex53
+/*
+Faça um programa para ler as dimensões de um terreno(comprimento c e largura l),
+bem como o preço por metro de tela p. Imprima o custo para cercar este mesmo terreno
+com tela.
+*/
