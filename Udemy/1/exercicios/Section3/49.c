e0bbedb5a6ad0c80175d724455d74913888b46c0
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-16 18:32:58 -0300

    solving

---
+++ Udemy/1/exercicios/Section3/49.c
@@ -0,0 +1,6 @@
+//Ex49
+/*
+Faça um programa que leia o horário (hora, minuto e segundo) de inicio 
+e duração, em segudos, de uma experiência biológica. O pgroama deve resultar com 
+o novo horário(hora, minuto e segudno) do término da mesma.
+*/
---
+++ Udemy/1/exercicios/Section3/50.c
@@ -0,0 +1,6 @@
+//Ex50
+/*
+Implemente um programa que calcule o ano de nascimento de uma pessoa a partir de 
+sua idade atual.
+*/
+
