c6b8da5235bd4be11aa3ade478c021b77ef31431
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-16 18:42:12 -0300

    solved 50.c

--- Udemy/1/exercicios/Section3/50.c
+++ Udemy/1/exercicios/Section3/50.c
@@ -4,3 +4,17 @@ Implemente um programa que calcule o ano de nascimento de uma pessoa a partir de
 sua idade atual.
 */
 
+#include <stdio.h>
+
+int main()
+{
+    int age, currentYear, birthYear;
+
+    printf("Quantos anos voce tem? ");
+    scanf("%d", &age);
+    printf("Em que ano estamos? ");
+    scanf("%d", &currentYear);
+
+    birthYear = currentYear - age;
+    printf("%d", birthYear);
+}
