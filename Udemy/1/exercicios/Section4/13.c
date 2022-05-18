b2d6434317ec8bca66ec5bf17353e9432c1a2ac3
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-17 22:41:59 -0300

    solved 13

--- Udemy/1/exercicios/Section4/13.c
+++ Udemy/1/exercicios/Section4/13.c
@@ -6,3 +6,31 @@ do aluno e indicar se o aluno foi aprovado ou reprovado. A nota para aprovação
 deve ser igual ou superior a 60 pontos.
 */
 
+#include <stdio.h>
+
+int main()
+{
+    float nota1, nota2, nota3, ponderada;
+    int peso1, peso2;
+
+    peso1 = 2;
+    peso2 = 8;
+    printf("Digite a nota da prova 1: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a nota da prova 2: ");
+    scanf("%f", &nota2);
+
+    printf("Digite a nota da prova 3: ");
+    scanf("%f", &nota3);
+
+    ponderada = peso1 * nota1 + peso1 * nota2 + peso2 + nota3 / peso1 + peso2;
+    printf("A nota e: %f", ponderada);
+
+    if(ponderada >= 60)
+    {
+        printf(" e voce esta aprovado.");
+    }
+    else
+        printf(" e voce esta reprovado.");
+}
