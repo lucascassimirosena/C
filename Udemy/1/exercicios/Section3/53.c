646e5b823037183f3a0d144a78d6448bfefd576d
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-17 22:22:35 -0300

    solved 53

--- Udemy/1/exercicios/Section3/53.c
+++ Udemy/1/exercicios/Section3/53.c
@@ -4,3 +4,23 @@ Faça um programa para ler as dimensões de um terreno(comprimento c e largura l
 bem como o preço por metro de tela p. Imprima o custo para cercar este mesmo terreno
 com tela.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    float c, l, cost, price, size;
+
+
+    printf("Insira o comprimento: ");
+    scanf("%f", &c);
+    printf("Insira a largura: ");
+    scanf("%f", &l);
+    printf("Qual e o valor do metro? ");
+    scanf("%f", cost);
+
+    size = c * l;
+    price = size * cost;
+
+    printf("%f", price);
+}
