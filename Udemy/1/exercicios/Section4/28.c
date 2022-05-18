b7046525c2979713853c6c08a448d82641f17c07
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-17 23:12:10 -0300

    solving

---
+++ Udemy/1/exercicios/Section4/28.c
@@ -0,0 +1,57 @@
+//EXx27
+/*
+Faça um programa que leia três números inteiros positivos e efetue o cáluclo de uma das seguintes médias de acordo com um valor numérido digitado
+pelo usúario.
+*/
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    int num1, num2, num3, result;
+    printf("Digite um numero: ");
+    scanf("%d", &num1);
+    printf("Digite um numero: ");
+    scanf("%d", &num2);
+    printf("Digite um numero: ");
+    scanf("%d", &num3);
+
+    int value;
+    printf("Gostaria de calcular qual tipo de media?\n");
+    printf(" 1 - Geometrica\n 2 - Ponderada\n 3 - Harmonica\n 4 - Aritmetica\n");
+    printf("Opcao: ");
+    scanf("%d", &value);
+
+    int mult, pond, arit;
+    switch(value)
+    {
+        case 1:
+        mult = num1 * num2 * num3;
+        result = pow(mult,1.0/3.0);
+        printf("%d", result);
+        break;
+
+        case 2:
+        pond = num1 + 2 * num2 + 3 * num3 / 6;
+        printf("%d", pond);
+        break;
+
+        case 3:
+        printf("Resolver harmonica");
+        break;
+
+        case 4:
+        arit = num1 + num2 + num3 / 3;
+        printf("%d", arit);
+        break;
+
+        default:
+        printf("Invalid");
+
+
+        
+    }
+}
+
+//INCOMPLETA, valor do case 4 esta errado.
