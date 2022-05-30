ee31367228b1e967d7b99d5ea2554716c5cce2d0
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-30 20:27:55 -0300

    solving

--- Udemy/1/exercicios/Section5/16.c
+++ Udemy/1/exercicios/Section5/16.c
@@ -4,3 +4,23 @@ Faça um programa que leia um número inteiro positivo ímpar N e imprima todos
 os números ímpares de 1 até N em ordem decrescente.
 */
 
+#include <stdio.h>
+int main()
+{
+    int num, odd;
+
+    printf("Digite um numero impar: ");
+    scanf("%d", &num);
+    
+    odd = num - 2;
+    while(odd > num)
+    {
+        num = num - 2;
+        printf("%d", odd);
+        printf(" ");
+    }
+}
+
+
+// I had to stop this study because the class started
+// CONTINUE
