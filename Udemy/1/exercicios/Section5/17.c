f8d98c2b6399e5ad0a3f12f690f8495f1ce760ae
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-31 19:23:51 -0300

    solving exercises

---
+++ Udemy/1/exercicios/Section5/17.c
@@ -0,0 +1,29 @@
+//Ex17
+/*
+Faça um programa que leia um número inteiro positivo N e calcule a soma dos N 
+primeiros números naturais
+*/
+
+#include <stdio.h>
+int main()
+{
+    int num, sum, i, inum;
+
+    printf("Digite umn numero: ");
+    scanf("%d", &num);
+
+    i = 0;
+    inum = i + 1;
+    do
+    {
+        sum = i + inum;
+        i++;
+        inum++;
+    } while (i < num );
+    
+    printf("%d", sum);
+
+}
+
+
+//Check it again
---
+++ Udemy/1/exercicios/Section5/18.c
@@ -0,0 +1,15 @@
+//Ex18
+/*
+Escreva um algoritmo que leia certa quantidade de números e imprima o maior
+deles e quantas vezes o maior número foi lido. A quantidade de números a 
+serem lidos deve ser fornecida pelo usuário.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num, times, i;
+
+    
+}
