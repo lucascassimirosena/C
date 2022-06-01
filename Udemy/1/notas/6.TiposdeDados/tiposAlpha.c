c7bac32b60ae90b35c2a8bd4593c5298767fbc6a
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-06-01 14:55:00 -0300

    studying

---
+++ Udemy/1/notas/6.TiposdeDados/tiposAlpha.c
@@ -0,0 +1,21 @@
+/*
+
+************ TIPOS ALFA NUMÉRICOS*******
+- Characteres;
+- Strings*
+
+* Na linguagem C, não existe o tipo de dado String.
+"ASFLASJFKLSJDDFLSAJ" - String
+'c' - Caractere
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    
+
+    return (0);
+}
