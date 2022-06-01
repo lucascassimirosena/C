f7526e67fb78ee823fdc76e05fcd2e76b0c82c6a
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-05-31 22:37:31 -0300

    added info

---
+++ Udemy/1/exercicios/Section5/20.c
@@ -0,0 +1,6 @@
+//Ex20
+/*
+Ler uma sequência de números e determinar se eles são pares ou não. Deverá
+ser ifnormado o número de dados lidos e número de valores pares. O processo termina
+quando for digitado o número 1000.
+*/
