ac3f0c93ef3f0158633069723fab5b3f81c52e2b
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-06-02 11:30:58 -0300

    updated

--- Udemy/1/notas/6.TiposdeDados/math.c
+++ Udemy/1/notas/6.TiposdeDados/math.c
@@ -8,6 +8,23 @@ resto %
 dividir /
 elevar ao quadrado x ** 2
 
-
-não vou nem fazer exemplo porque é extremamente fácil.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    //Verificar se um número é ímpar ou par em C
+    printf("Digite um numero: ");
+    scanf("%d", &num1);
+
+    if(num1 % 2 == 0)
+    {
+        printf("%d e par", num1);
+    }
+    else
+    {
+        printf("%d e impar", num1);
+    }
+}
