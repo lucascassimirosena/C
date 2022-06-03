0d396869e76f886db7dfbceaea811ac19bbb6f9a
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-06-03 17:57:02 -0300

    solving

--- Udemy/1/exercicios/Section5/20.c
+++ Udemy/1/exercicios/Section5/20.c
@@ -4,3 +4,33 @@ Ler uma sequência de números e determinar se eles são pares ou não. Deverá
 ser ifnormado o número de dados lidos e número de valores pares. O processo termina
 quando for digitado o número 1000.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3, sum, res;
+
+
+    while(num1 != 1000)
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &num1);
+        printf("Digite um numero: ");
+        scanf("%d", &num2);       
+
+        sum = 0;
+        if(num1 % 2 == 0)
+        {
+            sum = num1 + num2;
+            num3 = sum;
+
+        }
+
+        
+    }
+    res = num3 + sum;
+    printf("%d", num1);
+}
+
+//solving
