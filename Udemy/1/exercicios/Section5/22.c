1bb3f1b40d5db781eb8f80dcee4f3f241c056b98
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-06-03 17:59:51 -0300

    solving

---
+++ Udemy/1/exercicios/Section5/22.c
@@ -0,0 +1,7 @@
+//Ex22
+/*
+Escreva um programa completo que permita a qualquer aluno introduzir, por teclado, uma sequência arbitraria de notas (válidas no intervalo de 10 a 20)
+e que mostre na tela como resultado, a correspondente média aritmética. O número de notas com que o aluno
+pretenda efetuar o cálculo não será fornecido ao programa, o qual terminará quando for introduzido um valor que não seja válido
+como nota de aprovação.
+*/
