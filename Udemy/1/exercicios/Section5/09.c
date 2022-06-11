c69062bb9feba1c093b3928ffee63aff9d1f9703
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-06-10 21:28:36 -0300

    solved

--- Udemy/1/exercicios/Section5/09.c
+++ Udemy/1/exercicios/Section5/09.c
@@ -5,22 +5,23 @@ Make a program that reads the int number N and print the first N natural odd num
 
 #include <stdio.h>
 
+
 int main()
 {
-    int num = 0, i = 0, num2;
+    int num = 0, i = 0, odd = 1;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    for(i = 0; i <= num; i++)
-    {
-        if(num % 2 == 0)
-        printf(" ");
-        else 
-        num2 = num + 1 - num;
-        printf("%d", num2);
-    }
+        while(i <= num)
+        {
+            if(num % 2 != 0)
+            {
+            printf("%d\n", odd);
+            odd = odd + 2;
+            i++;
+            }
+        }
 
 }
-
 
