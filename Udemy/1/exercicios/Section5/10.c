41f3adfe005d01a101d3b954caf768ddad7a72f1
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-06-10 21:39:45 -0300

    solved

--- Udemy/1/exercicios/Section5/10.c
+++ Udemy/1/exercicios/Section5/10.c
@@ -3,3 +3,28 @@
 Faça um programa que calcule e mostre a soma dos 50 primeiros números pares.
 */
 
+#include <stdio.h>
+
+int main()
+{
+int n, i, even = 0, sum = 0;
+
+do
+{
+    if (n > 0)
+    {
+        if (n % 2 == 0)
+        {
+            printf("Numero par: %d\t",even);
+            sum = sum + even;
+            printf("A soma e:%d\n", sum);
+            even = even + 2;
+            i = i + 1;
+        }        
+    }
+}
+while(i <= 50);
+
+    return 0;
+
+}
