b8a4c062e5e54c3b1cabc79f2fcd95db21035a92
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-06-12 00:02:14 -0300

    solving

--- Udemy/1/exercicios/Section7/02.c
+++ Udemy/1/exercicios/Section7/02.c
@@ -8,13 +8,33 @@ valores lidos.
 int main()
 {
     int value[5];
+    int v0, v1, v2, v3, v4, v5;
 
-
+    v0 = value[0];
     printf("Digite um valor: ");
     gets(value[0]);
 
+    v1 = value[1];
+    printf("Digite um valor: ");
+    gets(value[1]);
+
+    v2 = value[2];
+    printf("Digite um valor: ");
+    gets(value[2]);
+
+    v3 = value[3];
+    printf("Digite um valor: ");
+    gets(value[3]);
+
+    v4 = value[4];
+    printf("Digite um valor: ");
+    gets(value[4]);
+
+    v5 = value[5];
+    printf("Digite um valor: ");
+    gets(value[5]);
 
 
-    printf("%d", value[0]);
-    
+    printf("%d", v0);
+
 }
