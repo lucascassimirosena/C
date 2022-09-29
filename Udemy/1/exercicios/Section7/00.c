d1ef52a17e3eb1d5061f2df8d13ab6edd6e349fd
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-09-29 10:24:56 -0300

    returned to the code

--- Udemy/1/exercicios/Section7/00.c
+++ Udemy/1/exercicios/Section7/00.c
@@ -13,12 +13,21 @@ int main()
 {
     //Vetores e strings
     char nome[50];
+    // sting é qualquer coisa entre " ". como: "Informaticien"
     printf("Qual seu nome? ");
     gets(nome);
     printf("Ola %s", nome);
 
     //vetores e caracteres
     char letras[26];
+    // caracteres são qualquer coisa dentro de aspas simples como 'b' 'c' 'd' '1' ...
+    int contador = 0;
+    for(int i = 97; i <- 122; i++) 
+    {
+        letras[contador] = i;
+        contador = contador + 1;
+    }
+
 
     //vetores de inteiros
     int numeros[10];
@@ -26,6 +35,11 @@ int main()
     //vetores e reais
     float valores[5];
 
+    //imprimindo as letras e seus valores em decimal
+    for(int i =0; i < 26; i++)
+    {
+        printf("%d == %c\n", letras[i], letras[i]);
+    }
 
 
 
