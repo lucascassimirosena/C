13860c207d990733bcb8fab50bca6f7fff4b3bf0
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-09-29 09:48:12 -0300

    testing and return the C studies!

---
+++ Udemy/1/exercicios/Section7/00.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+// Vetores
+
+// Array uni-dimensional
+/* 
+
+char nome[5];
+
+*/
+
+int main()
+{
+    //Vetores e strings
+    char nome[50];
+    printf("Qual seu nome? ");
+    gets(nome);
+    printf("Ola %s", nome);
+
+    //vetores e caracteres
+    char letras[26];
+
+    //vetores de inteiros
+    int numeros[10];
+
+    //vetores e reais
+    float valores[5];
+
+
+
+
+    return 0;
+}
